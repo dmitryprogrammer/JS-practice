f08785f945669d35f577ec58e494ad7196996806
dpischalka <dpischalka@gmail.com>
2018-03-20 16:58:49 +0400

    practices with generators.

--- src/generators/index.js
+++ src/generators/index.js
@@ -20,6 +20,16 @@ export function* generateAlphaNum() {
     yield* generateSequence(97, 122);
 }
 
+//  ping pong generator
+export function* pingPongGenerator() {
+    let yield1 = yield 1;
+    console.log("variable1", yield1);
+    let yield2 = yield 2;
+    console.log("variable2", yield2);
+    let yield3 = yield 3;
+    console.log("variable2", yield3);
+}
+
 export function main() {
     let firstGeneratorTrying = firstGenerator();
     for (let value of firstGeneratorTrying) {
@@ -33,4 +43,10 @@ export function main() {
     }
 
     console.log(str);
+
+    let pingPongGeneratorExample = pingPongGenerator();
+    console.log(pingPongGeneratorExample.next().value);
+    console.log(pingPongGeneratorExample.next(5).value);
+    console.log(pingPongGeneratorExample.next(10).value);
+    console.log(pingPongGeneratorExample.next(15).done);
 }
